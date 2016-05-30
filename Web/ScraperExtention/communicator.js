2f6fbab119353a88b019b450cfe57a37fcb83012
vruyror <vruyror@ogmainc.com>
2016-05-30 19:29:54 +0400

    Add global scenario logic

--- Web/ScraperExtention/communicator.js
+++ Web/ScraperExtention/communicator.js
@@ -1,61 +1,147 @@
 ﻿var document_status = {
+    moleCanScrap: false,
     isReady: false,
     tabId: -1,
     scenarioLoaded: false,
     isDataPostedToMole: true,
-    storeData: null
+    gloabalScenario: null,
+    storeData: null,
+    statusCodes: new Array(),
+    lastStatusCode: null,
+    task: {
+        taskKey: "",
+        file: "",
+        channel: "",
+        location: "",
+        dateFrom: "",
+        dateTo: "",
+        checkPropId: 0,
+        properties: new Array()
+    }
+};
+
+var scraper_status_codes = {
+    scraper_can_scrap: {
+        type: 0,
+        status: 1,
+        message: "Scraper can scrap...",
+        time: null
+    },
+    scraper_can_start: {
+        type: 0,
+        status: 2,
+        message: "Scraper can start process...",
+        time: null
+    },
+    scraper_global_scenario: {
+        type: 0,
+        status: 3,
+        message: "Global scenario has loaded...",
+        time: null
+    },
+    scraper_read_task: {
+        type: 0,
+        status: 4,
+        message: "Start read task...",
+        time: null
+    },
+    scraper_finish_read_task: {
+        type: 0,
+        status: 5,
+        message: "End read task...",
+        time: null
+    },
+    scraper_start: {
+        type: 0,
+        status: 6,
+        message: "Start scraping...",
+        time: null
+    }
 };
 
 var actions = {
     test: "test",
-    document_status: "document_status",
-    data_post_save: "data_post_save"
+    set_document_status: "set_document_status",
+    get_document_status: "get_document_status",
+    status_message: "status_message",
+    send_popup: "send_popup",
+    load_gloabal_scenario: "load_gloabal_scenario"
 };
+
+
+//var docStatis = document_status;
 
 //util
 
 function Communicator(){
     var o = this;
+    this.$loadScenario = $("<div></div>");
 
-    this.loadScenario = function (docStatus) {
-        var $loadScenario = $("<div></div>");
-        $loadScenario.load("Scenario/Expedia/expedia_scenario.json", function (response, status, xhr) {
+    //load resources
+    this.loadGlobalScenario = function (tab, sendResponse) {
+        var docStatus = o.getDocumentStatus(tab);
+        o.$loadScenario.load("scenario/scraping_scenario.json", function (response, status, xhr) {
             if (status == "success") {
                 var scenarioJson = JSON.parse(response.toString());
-                docStatus.storeData = scenarioJson;
-                docStatus.scenarioLoaded = true;
-                dataStore.updateData(docStatus.tabId, docStatus);
+                docStatus.gloabalScenario = scenarioJson;
+                o.setDocumentStatus(tab, docStatus);
+                docStatus = o.getDocumentStatus(tab);
+                o.setStatus(tab, scraper_status_codes.scraper_global_scenario);
+                docStatus = o.getDocumentStatus(tab);
+                sendResponse(o.getDocumentStatus(tab));
             }
             else {
-
+                //error code
             }
         });
     };
 
-    //listener methods
+    //listener method
     chrome.extension.onRequest.addListener(function (request, sender, sendResponse) {
         switch (request.action) {
             case actions.test:
                 sendResponse(actions.test);
                 break;
-            case actions.document_status:
-                if (request.data != null) {
-                    request.data.tabId = sender.tab.id;
-                    dataStore.updateData(sender.tab.id, request.data);
-                    if (request.data.isReady && !request.data.scenarioLoaded) {
-                        //load scenario
-                        o.loadScenario(request.data);
-                    }
-                }
-                var data = communicator.getDocumentStatus(sender.tab.id);
-                sendResponse(data);
+            case actions.set_document_status:
+                o.setDocumentStatus(sender.tab, request.data);
+                sendResponse(o.getDocumentStatus(sender.tab));
                 break;
-            case actions.data_post_save:
-                if (request.data != null) {
-                    request.data.tabId = sender.tab.id;
-                    dataStore.setData(sender.tab.id, request.data);
-                }
+            case actions.get_document_status:
+                sendResponse(o.getDocumentStatus(sender.tab));
                 break;
+            case actions.status_message:
+                o.setStatus(sender.tab, request.data);
+                sendResponse(o.getDocumentStatus(sender.tab));
+                break;
+            case actions.load_gloabal_scenario:
+                o.loadGlobalScenario(sender.tab, sendResponse);
+                break;
+
+            
+            //case actions.document_status:
+            //    if (request.data != null) {
+            //        request.data.tabId = sender.tab.id;
+            //        dataStore.updateData(sender.tab.id, request.data);
+            //        if (request.data.isReady && !request.data.scenarioLoaded) {
+            //            //load scenario
+            //            o.loadScenario(request.data);
+            //        }
+            //    }
+            //    var data = communicator.getDocumentStatus(sender.tab.id);
+            //    sendResponse(data);
+            //    break;
+            //case actions.data_post_save:
+            //    if (request.data != null) {
+            //        request.data.tabId = sender.tab.id;
+            //        dataStore.setData(sender.tab.id, request.data);
+            //    }
+            //    break;
+            //case actions.status:
+            //    var docStatus = dataStore.getData(sender.tab.id);
+            //    docStatus.statusLine = "\n" + request.data;
+            //    dataStore.setData(sender.tab.id, docStatus);
+            //    o.sendToPopup(request);
+            //    break;
         }
     });
 
@@ -81,6 +167,45 @@ function Communicator(){
     };    
 
     //get methods
+    this.getIdentification = function (tab) {
+        if (tab != null) {
+            return tab.id;
+        }
+        return null;
+    };
+
+    this.getDocumentStatus = function (tab) {
+        var identification = o.getIdentification(tab);
+        if (identification != null) {
+            return dataStore.getData(identification);
+        };
+        return null;
+    };
+
+    //set methods
+    this.setDocumentStatus = function (tab, data) {
+        var identification = o.getIdentification(tab);
+        if (identification != null) {
+            data.tabId = identification;
+            dataStore.setData(tab.id, data);
+        }
+    };
+
+    this.setStatus = function (tab, newStatus) {
+        newStatus.time = new Date().getTime();
+        var docStatus = o.getDocumentStatus(tab);
+        if (docStatus.lastStatusCode == null) {
+            docStatus.statusCodes[docStatus.statusCodes.length] = newStatus;
+            docStatus.lastStatusCode = newStatus;
+        }
+        else if (docStatus.lastStatusCode.status < newStatus.status) {
+            docStatus.statusCodes[docStatus.statusCodes.length] = newStatus;
+            docStatus.lastStatusCode = newStatus;
+        }
+        o.setDocumentStatus(tab, docStatus);
+        o.sendToPopup({ action: actions.send_popup, data: docStatus });
+    };
+    /*
     this.getDocumentStatus = function (currentTabId, onResponce) {
         if (currentTabId == null) {
             if (onResponce != null) {
@@ -97,7 +222,7 @@ function Communicator(){
         }
         return docStatus;
     };
-
+    */
     this.getNewData = function (onDocStatusChanged) {
         window.setTimeout(function () {
             o.sendToCommunicator({ action: actions.document_status, data: null }, function (responce) {
--- Web/ScraperExtention/mole-popup.js
+++ Web/ScraperExtention/mole-popup.js
@@ -1,11 +1,7 @@
-﻿var currentTabId = null;
+﻿/*
+var currentTabId = null;
 
-//window.onload = function () {
-//    chrome.tabs.getSelected(function (o) {
-//        currentTabId = o.id;
-//        ready();
-//    });
-//};
+
 
 function ready() {
     $("#getTabId").click(function () {
@@ -29,17 +25,25 @@ function ready() {
         alert(document_status.storeData);
     });
 
+
+    
+
     document_status = communicator.getDocumentStatus(currentTabId);
     setPopupCondition();
     
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-        if (request.action == actions.document_status) {
-            if (document_status.isReady != request.data.isReady) {
-                if (currentTabId == request.data.tabId) {
-                    document_status = request.data;
-                    setPopupCondition();
+        switch (request.action) {
+            case actions.document_status:
+                if (document_status.isReady != request.data.isReady) {
+                    if (currentTabId == request.data.tabId) {
+                        document_status = request.data;
+                        setPopupCondition();
+                    }
                 }
-            }
+                break;
+            case actions.status:
+                $("#actions_status").val($("#actions_status").val() + "\n" + request.data);
+                break;
         }
     });
     
@@ -56,6 +60,9 @@ function setStatus(statusText) {
 };
 
 function setPopupCondition() {
+    alert(document_status.statusLine);
+    $("#actions_status").val(document_status.statusLine);
+
     $("#btn").prop("disabled", true);
 
     $("#storeData").val("");
@@ -78,11 +85,85 @@ function setPopupCondition() {
     }
 };
 
-//function CheckStatus
+*/
+
+function MolePopup(identification) {
+    var o = this;
+    this.identification = identification;
+    this.dataStore = new DataStore();
+    this.communicator = new Communicator();
+    this.$statusArea = $("#statusArea");
+    this.$btnDisplayStatus = $("#btnDisplayStatus");
+    this.$btnRemoveStatus = $("#btnRemoveStatus");
+    this.$btnReload = $("#btnReload");
+    this.$txtTaskKey = $("#txtTaskKey");
+    this.$txtFile = $("#txtFile");
+    this.$txtChannel = $("#txtChannel");
+    this.$txtLocation = $("#txtLocation");
+    this.$txtDateFrom = $("#txtDateFrom");
+    this.$txtDateTo = $("#txtDateTo");
+
+    this.ready = function () {
+        o.displayCurrentStatus();
+        o.$btnDisplayStatus.unbind("click", o.displayCurrentStatus);
+        o.$btnDisplayStatus.bind("click", o.displayCurrentStatus);
+        o.$btnRemoveStatus.unbind("click", o.removeCurrentStatus);
+        o.$btnRemoveStatus.bind("click", o.removeCurrentStatus);
+        o.$btnReload.unbind("click", o.reload);
+        o.$btnReload.bind("click", o.reload);
+    };
+
+    this.displayCurrentStatus = function () {
+        var ststusLine = "";
+        o.$statusArea.val(ststusLine);
+        var data = o.dataStore.getData(o.identification);
+        for (var ststusIndex = 0; ststusIndex < data.statusCodes.length; ststusIndex++) {
+            var status = data.statusCodes[ststusIndex];
+            var message = status.message + " " + status.time;
+            if (ststusLine == "") {
+                ststusLine = message;
+            }
+            else {
+                ststusLine = ststusLine + "\n" + message;
+            }
+        }
+        o.$statusArea.val(ststusLine);
+
+        o.$txtTaskKey.val(data.task.taskKey);
+        o.$txtFile.val(data.task.file);
+        o.$txtChannel.val(data.task.channel);
+        o.$txtLocation.val(data.task.location);
+        o.$txtDateFrom.val(data.task.dateFrom);
+        o.$txtDateTo.val(data.task.dateTo);
+    };
+
+    this.removeCurrentStatus = function () {
+        var data = o.dataStore.getData(o.identification);
+        data.statusCodes = new Array();
+        o.dataStore.setData(o.identification, data);
+        o.displayCurrentStatus();
+    };
+
+    this.reload = function () {
+        o.removeCurrentStatus();
+        chrome.tabs.reload(o.identification);
+    };
+
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+        switch (request.action) {
+            case actions.send_popup:
+                if (request.data.tabId == o.identification) {
+                    o.displayCurrentStatus();
+                }
+                break;
+        }
+    });
+};
+
 
 document.addEventListener("DOMContentLoaded", function () {
     chrome.tabs.getSelected(function (o) {
-        currentTabId = o.id;
-        ready();
+        var molePopup = new MolePopup(o.id);
+        molePopup.ready();
     });
 });
